0854b04ac8f0d668d72e398892d19c965e49efbd
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-11-03 21:01:06 +0000

    Se generan rutas de logeo

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # Configuracion rutas users -> Solo se permite new y create 
   resources :users, only: [:new, :create]
   
+  # Configuracion rutas login new, create y logout
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
