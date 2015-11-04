15872d04ee9494fa743d538c41228a39a61b462d
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-11-04 14:01:08 +0000

    Se genera funcionalidad de un nuevo post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all.order("created_at DESC")
     end
+    
+    def new
+        @article = Article.new
+    end
 end
